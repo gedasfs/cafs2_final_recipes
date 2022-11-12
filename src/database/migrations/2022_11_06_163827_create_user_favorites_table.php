ada504911920fd33c5a148772c300d3f53e47690
gedas.fs <gedas.fs@gmail.com>
2022-11-12 15:46:38 +0200

    add timestamps to 'user_favorites' table

--- src/database/migrations/2022_11_06_163827_create_user_favorites_table.php
+++ src/database/migrations/2022_11_06_163827_create_user_favorites_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('recipe_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
-            // $table->timestamps();
+            $table->timestamps();
         });
     }
 
