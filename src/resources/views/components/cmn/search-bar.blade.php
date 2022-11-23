c0bc96fabf1485b7b099507e5ba5a66e6cc09815
gedas.fs <gedas.fs@gmail.com>
2022-11-23 11:20:54 +0200

    fix search bar (not coming back to original pos. in non-index pages)

--- src/resources/views/components/cmn/search-bar.blade.php
+++ src/resources/views/components/cmn/search-bar.blade.php
@@ -1,6 +1,16 @@
-<div {{ $attributes->merge(['class' => 'searchBar']) }}>
+@props(['routeIsIndex'])
+
+<div @class(['searchBar', 'search-bar-on-img' => $routeIsIndex])>
     <div class="input-group mt-3 mb-5">
-        <input size="10" type="text" class="form-control text-primary border-primary rounded-5 p-2 ps-3 p-lg-3 position-absolute end-0 w-100" placeholder="Atrask kažką skanaus..." aria-label="search" aria-describedby="basic-addon1">
-        <x-cmn.link-btn class="p-2 p-lg-3 px-3 px-lg-4 position-absolute end-0">Ieškoti</x-cmn.link-btn>
+        <input
+            type="text"
+            @class(['form-control', 'text-primary', 'border-primary', 'rounded-5', 'p-2', 'ps-3', 'p-lg-3' => $routeIsIndex])
+            placeholder="Atrask kažką skanaus..."
+            aria-label="search"
+            aria-describedby="basic-addon1"
+        >
+        <x-cmn.link-btn
+            @class(['p-2', 'p-lg-3' => $routeIsIndex, 'px-3', 'px-lg-4', 'position-absolute', 'end-0'])
+        >Ieškoti</x-cmn.link-btn>
     </div>
 </div>
--- src/resources/views/components/header/index.blade.php
+++ src/resources/views/components/header/index.blade.php
@@ -1,9 +1,13 @@
-<header class="position-relative">
+@php
+    $routeIsIndex = request()->route()->named('index');
+@endphp
+
+<header @class(['position-relative' => $routeIsIndex])>
     <x-nav />
-    @if (request()->route()->named('index'))
+    @if ($routeIsIndex)
         <div class="bg-light">
             <img class="landing-img rounded rounded-2" src={{ asset('images/header-img_board-on-metal.jpg') }} alt="landing-img">
         </div>
     @endif
-    <x-cmn.search-bar />
+    <x-cmn.search-bar :$routeIsIndex />
 </header>
