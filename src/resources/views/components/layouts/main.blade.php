43a0c0953946e6d58dbf1e1634b7266d6cd4b208
gedas.fs <gedas.fs@gmail.com>
2022-11-29 10:12:44 +0200

    Front-End. Fix footer (always show at the bottom)

--- src/resources/views/components/layouts/main.blade.php
+++ src/resources/views/components/layouts/main.blade.php
@@ -14,7 +14,7 @@
     @vite(['resources/js/app.js'])
 </head>
 <body>
-    <div class="container-fluid min-vh-100 topmost-container">
+    <div class="container-fluid d-flex flex-column min-vh-100 topmost-container">
         <x-header />
 
         <main class="mb-5">
