57bf0a6b936cfa11118d3db24e36b8b8317bfc76
gedas.fs <gedas.fs@gmail.com>
2022-11-29 20:56:56 +0200

    moved card-recipe blade file from cmn to recipes folder

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <article class="row justify-content-center">
             @foreach ($recipes as $recipe)
                 <div class="col-12 col-md-6 mb-3">
-                    <x-cmn.card-recipe :id="$recipe->id">
+                    <x-recipes.card-recipe :id="$recipe->id">
                         @php
                             $timeUnit = $timeUnits->first(fn($value, $key) => $value->id == $recipe->recipe_time_unit_id);
                             $difficultyLevel = $difficultyLevels->first(fn($value, $key) => $value->id == $recipe->difficulty_level_id)
@@ -17,7 +17,7 @@
                         <x-slot:servings>{{ $recipe->servings }}</x-slot:servings>
                         <x-slot:difficulty>{{ $difficultyLevel->name }}</x-slot:difficulty>
 
-                    </x-cmn.card-recipe>
+                    </x-recipes.card-recipe>
                 </div>
             @endforeach
         </article>
