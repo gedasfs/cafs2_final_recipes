590d8f2e82d24df1865b7581d380c98f7116b3ae
gedas.fs <gedas.fs@gmail.com>
2022-11-29 20:50:49 +0200

    fixes in seeders and factories

--- src/database/seeders/RecipeSeeder.php
+++ src/database/seeders/RecipeSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Recipe;
+use App\Models\Ingredient;
+use App\Models\Instruction;
+use App\Models\IngredientGroup;
 use Illuminate\Database\Seeder;
+use App\Models\InstructionGroup;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RecipeSeeder extends Seeder
@@ -15,6 +19,26 @@ class RecipeSeeder extends Seeder
      */
     public function run()
     {
-        Recipe::factory(10)->create();
+
+        $recipes = Recipe::factory(10)->create();
+
+        foreach ($recipes as $recipe) {
+            IngredientGroup::factory(2)
+                ->has(Ingredient::factory()->count(2))
+                ->create([
+                    'recipe_id' => $recipe->id,
+                ]);
+
+            InstructionGroup::factory(2)
+                ->has(Instruction::factory()->count(2))
+                ->create([
+                    'recipe_id' => $recipe->id,
+                ]);
+        }
+
+        // Recipe::factory(10)
+        //     ->has(IngredientGroup::factory()->count(3))
+        //     ->has(InstructionGroup::factory()->count(5))
+        //     ->create();
     }
 }
