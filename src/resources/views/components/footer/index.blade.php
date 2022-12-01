cc69d4423715800b8a3f3f12bed275040e552fdb
gedas.fs <gedas.fs@gmail.com>
2022-12-01 19:04:09 +0200

    update footer with personal data

--- src/resources/views/components/footer/index.blade.php
+++ src/resources/views/components/footer/index.blade.php
@@ -1,5 +1,6 @@
 <footer class="text-center bg-light mt-auto">
-    <div class="text-center text-primary p-4">
-        <h4>© {{ date('Y') }}</h4>
+    <div class="text-center text-primary pt-3">
+        <h5>{{ date('Y') }}</h5>
+        <p class="p-0 ">CAFS2, Gediminas Špogis ©</p>
     </div>
 </footer>
