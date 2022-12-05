eec862a35dffaa06e9d349ad3503379fac790b14
gedas.fs <gedas.fs@gmail.com>
2022-12-05 11:29:46 +0200

    move images from public to storage/...

--- src/resources/views/components/header/index.blade.php
+++ src/resources/views/components/header/index.blade.php
@@ -8,7 +8,7 @@
     <x-nav />
     @if ($routeIsIndex)
         <div class="bg-light">
-            <img class="landing-img rounded rounded-2" src={{ asset('images/header-img_board-on-metal.jpg') }} alt="landing-img">
+            <img class="landing-img rounded rounded-2" src={{ asset('images/header/header-img.jpg') }} alt="landing-img">
         </div>
     @endif
     @if (!in_array(request()->route()->uri, $deniedRoutes))
