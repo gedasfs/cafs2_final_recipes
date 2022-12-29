6354e09109795075f1b7bdcc85b088e4a99942db
gedas.fs <gedas.fs@gmail.com>
2022-12-29 17:55:43 +0200

    small improvements in looks

--- src/resources/views/components/cmn/link-btn-fav.blade.php
+++ src/resources/views/components/cmn/link-btn-fav.blade.php
@@ -1,7 +1,7 @@
 @props(['filled' => false])
 
-<x-cmn.link-btn :attributes="$attributes" outlined color="danger" class="px-2 py-1 rounded-circle">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-heart-fill btn-fav" viewBox="0 0 16 16">
+<x-cmn.link-btn :attributes="$attributes" outlined color="danger" class="px-2 py-1 rounded-circle" title="Pridėti prie patikusių">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" class="bi bi-heart-fill btn-fav" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
     </svg>
 </x-cmn.link-btn>
--- src/resources/views/recipes/show.blade.php
+++ src/resources/views/recipes/show.blade.php
@@ -2,44 +2,46 @@
     <section class="container recipe-show-section align-item-center">
         <h1>{{ $recipe->name }}</h1>
         <article>
-            <div class="bg-light rounded px-2 pt-2 pb-4 text-center mb-2">
-                <div class="mb-2 text-end">
-                    <x-cmn.link-btn-fav class="ms-2" title="pridėti prie patikusių"/>
-                    <x-cmn.link-btn-pdf class="ms-2" href="{{ route('pdf.recipe', $recipe->id) }}" target=_blank title="atsisiųsti PDF" />
-                </div>
-                <div>
-                    <p class="px-1 px-md-4">{{ $recipe->short_description }}</p>
+            <div class="my-2 pt-3 pb-2 px-2 px-md-3 bg-light rounded text-center">
+                <div class="">
+                    <p class="m-0 mb-3">{{ $recipe->short_description }}</p>
                     <p class="m-0">Įkėlė <strong class="text-muted">{{ $recipe->user->firstname }} {{ $recipe->user->lastname }}</strong></p>
                     <p class="text-muted m-0">{{ $recipe->created_at->format('Y-m-d') }}</p>
                 </div>
-
-                @can('update', $recipe)
-                    <div class="user-actions d-flex justify-content-center">
-                        <x-cmn.link-btn class="mt-3 me-2" outlined href="{{ route('recipes.edit', $recipe->id) }}">Redaguoti</x-cmn.link-btn>
-                        <form method="POST" action="{{ route('recipes.destroy', $recipe->id) }}" class="delete-form">
-                            @csrf
-                            @method('DELETE')
-                            <x-cmn.btn type="submit" class="mt-3" outlined color="danger">Ištrinti</x-cmn.btn>
-                        </form>
-                    </div>
-                @endcan
-
+                <div class="user-actions mt-3 d-flex gap-2 justify-content-center align-items-center">
+                    @auth
+                        <x-cmn.link-btn-fav />
+                    @endauth
+                    @can('update', $recipe)
+                        <x-cmn.link-btn-edit href="{{ route('recipes.edit', $recipe->id) }}" />
+                    @endcan
+                    @can('delete', $recipe)
+                        <x-cmn.form-delete action="{{ route('recipes.destroy', $recipe->id) }}" />
+                    @endcan
+                    <x-cmn.link-btn-pdf href="{{ route('pdf.recipe', $recipe->id) }}" target="_blank" />
+                </div>
             </div>
-            <div class="mb-2">
+            <div class="my-2">
                 <x-cmn.image-slider :images="$recipe->images" :id="'recipeSlider'.$recipe->id"></x-cmn.image-slider>
             </div>
-            <div class="p-4 rounded mb-2">
-                <p class="m-0">{{ $recipe->description }}</p>
+            <div class="my-4 px-1">
+                <p class="m-0 text-justify">{{ $recipe->description }}</p>
             </div>
-            <div class="row bg-light p-4 rounded mb-2 text-center">
-                <div class="col-12 col-sm-6">
-                    <p class="mb-2 m-sm-0">Sudėtingumas: <strong class="text-primary">{{ $recipe->difficultyLevel->name}}</strong></p>
+            <div class="row mt-2 bg-light p-2 p-sm-3 rounded mb-2 text-center">
+                <div class="col-12 col-sm-4">
+                    <p class="m-0">Kategorija:</p>
+                    <p class="mb-2 m-sm-0"><strong class="text-primary">{{ $recipe->category->name }}</strong></p>
                 </div>
-                <div class="col-12 col-sm-6">
-                    <p class="m-0">Porcijų sk.: <strong class="text-primary">{{ $recipe->servings }}</strong></p>
+                <div class="col-12 col-sm-4">
+                    <p class="m-0">Sudėtingumas:</p>
+                    <p class="mb-2 m-sm-0"><strong class="text-primary">{{ $recipe->difficultyLevel->name}}</strong></p>
+                </div>
+                <div class="col-12 col-sm-4">
+                    <p class="m-0">Porcijų kiekis:</p>
+                        <p class="mb-2 m-sm-0"><strong class="text-primary">{{ $recipe->servings }}</strong></p>
                 </div>
             </div>
-            <div class="row bg-light p-4 rounded text-center">
+            <div class="row mb-2 bg-light p-2 p-sm-3 rounded text-center">
                 <div class="col-12 col-sm-4">
                     <p class="m-0">Paruošimo laikas:</p>
                     <p class="mb-2 m-sm-0"><strong class="text-primary">{{ $recipe->prep_time }} {{ $timeUnit->name }}</strong></p>
@@ -53,29 +55,33 @@
                     <p class="mb-2 m-sm-0"><strong class="text-primary">{{ $recipe->total_time }} {{ $timeUnit->name }}</strong></p>
                 </div>
             </div>
-            <div class="p-4 my-2">
+            <div class="mt-4">
                 <h3>Ingredientai</h3>
-
                 <ul class="list-group">
                     @foreach ($recipe->ingredients as $ingridient)
-                        <li class="list-group-item">
-                            <input class="form-check-input me-3" type="checkbox" id="ingredient{{ $loop->index+1 }}">
-                            <label class="form-check-label stretched-link" for="ingredient{{ $loop->index+1 }}">{{ $ingridient->quantity }} {{ $ingridient->unit }} {{ $ingridient->name }}</label>
+                        <li class="list-group-item ms-md-2 d-flex">
+                            <div>
+                                <input class="form-check-input me-3" type="checkbox" id="ingredient{{ $loop->index+1 }}">
+                            </div>
+                            <label class="form-check-label stretched-link" for="ingredient{{ $loop->index+1 }}">{{ $ingridient->name }}, {{ $ingridient->quantity }} {{ $ingridient->unit }}</label>
                         </li>
                     @endforeach
                 </ul>
             </div>
-            <div class="p-4 mb-2">
+            <div class="mt-4">
                 <h3>Gaminimo eiga</h3>
-
                 <ol class="list-group list-group-numbered list-group-flush">
                     @foreach ($recipe->instructions as $instruction)
-                        <li class="list-group-item">{{ $instruction->description }}</li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start px-0 ms-md-2">
+                            <div class="ms-2 me-auto text-justify">
+                                {{ $instruction->description }}
+                            </div>
+                        </li>
                     @endforeach
                 </ol>
 
             </div>
-            <div class="p-4 my-2">
+            <div class="mt-4">
                 <h3>Media</h3>
                 <p class="m-0">Išorinė recepto nuoroda:</p>
                 <x-cmn.link>{{ $recipe->ext_url }}</x-cmn.link>
